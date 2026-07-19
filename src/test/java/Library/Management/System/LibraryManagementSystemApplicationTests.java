a723a54b1f035ccf19c5d8e60c2cbd1ae86affc3
vishal Nemlekar <nemlakerv@gmail.com>
2026-07-19 19:58:46 +0530

    first commit with setup

---
+++ src/test/java/Library/Management/System/LibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package Library.Management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
