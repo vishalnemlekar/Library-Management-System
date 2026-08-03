19b0c96b9a4d854a181d62e537b8030acd47e52e
vishal Nemlekar <nemlakerv@gmail.com>
2026-08-04 01:06:25 +0530

    added dto

---
+++ src/main/java/Library/Management/System/dto/BookResponse.java
@@ -0,0 +1,64 @@
+package Library.Management.System.dto;
+
+public class BookResponse {
+    private Long id;
+
+    private String title;
+    private String author;
+    private String isbn;
+    private Integer totalCopies;
+    private Integer availableCopies;
+
+    public BookResponse() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Integer getTotalCopies() {
+        return totalCopies;
+    }
+
+    public void setTotalCopies(Integer totalCopies) {
+        this.totalCopies = totalCopies;
+    }
+
+    public Integer getAvailableCopies() {
+        return availableCopies;
+    }
+
+    public void setAvailableCopies(Integer availableCopies) {
+        this.availableCopies = availableCopies;
+    }
+
+}
